75c488f68053f6c72963ca990393f9dcbc126f6f
James Pederson <james@jpederson.com>
2017-10-17 19:03:38 -0500

    Add noscript tag after each script tag. Strip 4.2 emoji script.

--- library/scripts.php
+++ library/scripts.php
@@ -8,4 +8,35 @@ function p_scripts() {
 add_action( 'wp_enqueue_scripts', 'p_scripts' );
 
 
+
+// add noscript tag in after each javascript that's enqueued.
+function add_noscript_filter( $tag, $handle, $src ){
+
+	$noscript = "<noscript>This site requires javascript to display/behave ideally, though the content will be accessible regardless.</noscript>\r\n";
+	$tag = $tag . $noscript; 
+	return $tag; 
+
+}
+add_filter( 'script_loader_tag', 'add_noscript_filter', 10, 3 );
+
+
+
+// disable emoticons
+function disable_wp_emojicons() {
+
+  // all actions related to emojis
+  remove_action( 'admin_print_styles', 'print_emoji_styles' );
+  remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+  remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+  remove_action( 'wp_print_styles', 'print_emoji_styles' );
+  remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+  remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+  remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
+
+  // filter to remove TinyMCE emojis
+  add_filter( 'tiny_mce_plugins', 'disable_emojicons_tinymce' );
+}
+add_action( 'init', 'disable_wp_emojicons' );
+
+
 ?>
