467c9e177c5258e126b2a9ff6a3d31818fe074ef
James Pederson <james@jpederson.com>
2022-03-03 15:23:28 -0600

    Remove audioeye tag from the footer since we're using a plugin now.

--- footer.php
+++ footer.php
@@ -47,6 +47,5 @@
 </div><!-- #container -->
 
 <?php wp_footer(); ?>
-<script type="text/javascript">!function(){var b=function(){window.__AudioEyeSiteHash = "69fbea092a425ba2a868178338935b1d"; var a=document.createElement("script");a.src="https://wsmcdn.audioeye.com/aem.js";a.type="text/javascript";a.setAttribute("async","");document.getElementsByTagName("body")[0].appendChild(a)};"complete"!==document.readyState?window.addEventListener?window.addEventListener("load",b):window.attachEvent&&window.attachEvent("onload",b):b()}();</script>
 </body>
 </html>
